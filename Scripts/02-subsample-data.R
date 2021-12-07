d8f90d62f2320bb686b108c1f18fa3760a7eb504
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2021-12-06 17:31:09 -0800

    Create function for resampling at different intervals

--- Scripts/02-subsample-data.R
+++ Scripts/02-subsample-data.R
@@ -1,10 +1,12 @@
 library(amt)
 library(adehabitatLT)
+library(dplyr)
+library(readr)
 
 # import all data and join into single dataframe
 igotu_cleaned <- list.files(path = "Data/Hunting/igotu_cleaned/",
                                pattern = "*.csv", full.names = TRUE, recursive = TRUE) %>% 
-    lapply(read.csv) %>% 
+    lapply(read_csv) %>% 
     bind_rows()
 
 # make tracks
@@ -14,27 +16,37 @@ hunter_tracks <- mk_track(tbl = igotu_cleaned,
                           .t = DateTime,
                           crs = CRS("+proj=longlat +ellps=WGS84")) 
 
-# subsample to 3 minute fix rate
-hunter_resampled <- hunter_tracks %>%
-    nest(data = -"id") %>%
-    mutate(resampled_data = map(data, function(x)
-        x %>% track_resample(rate = minutes(3),
-                             tolerance = seconds(0)))) %>%
-    select(id, resampled_data) %>%
-    unnest(cols = resampled_data) %>%
+# subsample to different fix rates
+resample_hunt_tracks <- function(minutes_resample) {
     
-    # change column names to match originals
-    rename(
-        Longitude = x_,
-        Latitude = y_,
-        DateTime = t_,
-        ID = id
-    )
+    hunter_resampled <- hunter_tracks %>%
+        nest(data = -"id") %>%
+        mutate(resampled_data = map(data, function(x)
+            x %>% track_resample(rate = minutes(minutes_resample),
+                                 tolerance = seconds(0)))) %>%
+        select(id, resampled_data) %>%
+        unnest(cols = resampled_data) %>%
+        
+        # change column names to match originals
+        rename(
+            Longitude = x_,
+            Latitude = y_,
+            DateTime = t_,
+            ID = id
+        )
+    
+    # join back the rest of the metadata to the resampled data
+    igotu_cleaned_resample <- left_join(hunter_resampled, igotu_cleaned) 
+    
+    # export cleaned data
+    write.csv(igotu_cleaned_resample, 
+              paste0("Data/igotu_data_", minutes_resample, "min.csv"), 
+              row.names=F)
+    
+}
 
-# join back the rest of the metadata to the resampled data
-igotu_cleaned_resample <- left_join(hunter_resampled, igotu_cleaned) 
+resample_hunt_tracks(1)
+resample_hunt_tracks(3)
+resample_hunt_tracks(5)
+resample_hunt_tracks(10)
 
-# export cleaned data
-write.csv(igotu_cleaned_resample, 
-          "Data/igotu_data_3min.csv", 
-          row.names=F)
