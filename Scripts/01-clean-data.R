65cee5bac17116c624fc87e65ad0d7d30c098c97
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2021-12-27 16:48:46 -0500

    Move clean-data from Rmd to R

---
+++ Scripts/01-clean-data.R
@@ -0,0 +1,79 @@
+# Clean iGotU Data
+
+library(dplyr)
+library(tidyr)
+library(hms)
+library(stringr)
+
+# import all data and join into single dataframe
+igotu_file_names <- list.files(path = "Data/Hunting/igotu_raw/",
+                               pattern = "*.csv", full.names = TRUE, recursive = TRUE) 
+
+# read all individual csvs into a list
+igotu_raw_dfs <- lapply(igotu_file_names, read.csv) 
+
+# make the file names into the object names in the list
+names(igotu_raw_dfs) <- igotu_file_names
+
+# combine all collar files into one dataframe
+igotu_raw <- bind_rows(igotu_raw_dfs, .id = "FileName")
+
+# clean columns
+igotu_data_all <- igotu_raw %>% 
+    mutate(DateTime = as.POSIXct(paste(Date, Time, sep = ""), 
+                                 "%m/%d/%y %H:%M:%S",
+                                 tz = "America/Los_Angeles"),
+           ID = str_replace(FileName, "Data/Hunting/igotu_raw//", ""),
+           ID = str_replace(ID, ".csv", "")) %>% 
+    select(-c(FileName, Altitude, Speed, Distance, Essential, Track, Course, Type))
+
+# add the metadata
+metadata <- read.csv(here::here("Data/Hunting/igotu_metadata_times_cleaned.csv")) %>% 
+    mutate(Start_time = paste0(Start_time, ":00"),
+           End_time = paste0(End_time, ":00")) %>% 
+    select(-Date)
+igotu_data_all <- left_join(igotu_data_all, metadata)
+
+# format time
+igotu_data_all$Time <- format(igotu_data_all$DateTime, format = "%H:%M:%S")
+
+# remove tracks with issues
+igotu_data_all <- igotu_data_all %>% 
+    filter(ID != "081416_12") %>% # gps error
+    filter(ID != "081316_12") %>% # inaccurate time
+    filter(ID != "082016_21") %>% # no movement
+    filter(ID != "082016_22") %>% # no movement
+    filter(ID != "080915_03") %>%  # no movement
+    filter(ID != "081416_18") %>% # no movement
+    filter(ID != "081917_38") # incomplete track (see issue #22)
+
+# remove points before start or after end, export cleaned data
+
+for(i in unique(igotu_data_all$ID)) {
+    
+    if(file.exists(paste0("Data/Hunting/igotu_cleaned/", i, ".csv"))) {
+        next
+    }
+    
+    # create temporary dataframe for each individual
+    temp_df <- filter(igotu_data_all, ID == i)
+    temp_df$Keep <- "NO"
+    
+    # filter to points within start and end
+    for(j in 1:nrow(temp_df)) {
+        if(as_hms(temp_df$Time[j]) > as_hms(temp_df$Start_time[j]) &&
+           as_hms(temp_df$Time[j]) < as_hms(temp_df$End_time[j])) {
+            temp_df$Keep[j] <- "YES"
+        }
+    }
+    
+    temp_df <- temp_df %>% 
+        # remove dropped points
+        filter(Keep == "YES") %>% 
+        # only take columns of interest
+        select(ID, Latitude, Longitude, Date, Time, DateTime, Party_ID, Harvest)
+    
+    write.csv(temp_df, paste0("Data/Hunting/igotu_cleaned/", i, ".csv"), row.names = F)
+    
+    
+}
