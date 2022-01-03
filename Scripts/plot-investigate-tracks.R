44b50e5d34c0c121dc3ef10c890c772e16e809a3
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2022-01-03 15:26:53 -0800

    Export plots for 3min HMM cleaned data

--- Scripts/plot-investigate-tracks.R
+++ Scripts/plot-investigate-tracks.R
@@ -91,3 +91,43 @@ for (i in 1:length(igotu_plots)) {
     print(igotu_plots[[i]])
 }
 dev.off()
+
+
+
+# CLEANED FOR HMM 3 MIN ---------------------------------------------------
+
+igotu_cleaned <- read.csv("Data/igotu_data_3min_covariates_for_hmm.csv")
+
+igotu_sf <- st_as_sf(igotu_cleaned,
+                     coords = c("x", "y"),
+                     crs = "+proj=longlat +ellps=WGS84")
+
+# bring in shape file
+hrec_shp <- st_read("Data/Spatial data/Raw from Alex/HREC_boundary.shp") 
+
+## Plots by individual animal
+igotu_cleaned$DateTime <- as.POSIXct(igotu_cleaned$DateTime)
+
+igotu_plots <- lapply(sort(unique(igotu_sf$ID)), function(i) {
+    
+    data_to_plot <- igotu_sf[igotu_sf$ID==i,]
+    date_breaks <- diff(range(data_to_plot$DateTime)) * 0:4 / 4 + min(data_to_plot$DateTime)
+    date_labels <- format(date_breaks, "%Y-%m-%d %H:%M:%S")
+    
+    ggplot() + 
+        geom_sf(data = hrec_shp) +
+        geom_sf(data = data_to_plot, aes(col = as.numeric(DateTime))) +
+        theme_bw() +
+        ggtitle(i) +
+        scale_colour_gradientn(colours=rev(rainbow(6)),
+                               breaks = as.numeric(date_breaks),
+                               labels = date_labels)
+})
+
+# print all plots to pdf
+pdf("igotu-plots-3min-hmm.pdf")
+for (i in 1:length(igotu_plots)) {
+    print(igotu_plots[[i]])
+}
+dev.off()
+
