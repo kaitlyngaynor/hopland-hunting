7e74cf759c4bd7ffd308bfb103dc2d6694dff62e
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2022-05-02 14:53:38 -0700

    Calculate time from sunrise & sunset

--- Scripts/01-clean-data.R
+++ Scripts/01-clean-data.R
@@ -5,6 +5,7 @@ library(tidyr)
 library(hms)
 library(stringr)
 library(sf)
+library(suncalc)
 
 # import all data and join into single dataframe
 igotu_file_names <- list.files(path = "Data/Hunting/igotu_raw/",
@@ -31,8 +32,21 @@ igotu_data_all <- igotu_raw %>%
 # add the metadata
 metadata <- read.csv(here::here("Data/Hunting/igotu_metadata_times_cleaned_03Feb2022.csv")) %>% 
     mutate(Start_time = paste0(Start_time, ":00"),
-           End_time = paste0(End_time, ":00")) %>% 
+           End_time = paste0(End_time, ":00"),
+           Date = as.Date(Date, format = "%m/%d/%y"))
+
+# determine sunrise & sunset time
+suntimes <- getSunlightTimes(date = unique(metadata$Date), 
+                 lat = 39.012, lon = -123.079,
+                 keep = c("sunrise", "sunset"), 
+                 tz = "America/Los_Angeles") %>% 
+    select(date, sunrise, sunset) %>% 
+    rename(Date = date, Sunrise = sunrise, Sunset = sunset)
+
+metadata <- left_join(metadata, suntimes) %>% 
     select(-Date)
+
+# join metadata
 igotu_data_all <- left_join(igotu_data_all, metadata)
 
 # format time
@@ -53,6 +67,8 @@ for(i in unique(igotu_data_all$ID)) {
     temp_df <- filter(igotu_data_all, ID == i)
     temp_df$Keep <- "NO"
     temp_df$Elapsed_Time <- NA
+    temp_df$Elapsed_Time_Sunrise <- NA
+    temp_df$Elapsed_Time_Sunset <- NA
     
     # filter to points within start and end
     for(j in 1:nrow(temp_df)) {
@@ -62,6 +78,12 @@ for(i in unique(igotu_data_all$ID)) {
             temp_df$Elapsed_Time[j] <- as.numeric(difftime(as_hms(temp_df$Time[j]),
                                                            as_hms(temp_df$Start_time[j]),
                                                            units = "hours"))
+            temp_df$Elapsed_Time_Sunrise[j] <- as.numeric(difftime(as_hms(temp_df$Time[j]),
+                                                           as_hms(temp_df$Sunrise[j]),
+                                                           units = "hours"))
+            temp_df$Elapsed_Time_Sunset[j] <- as.numeric(difftime(as_hms(temp_df$Sunset[j]),
+                                                                   as_hms(temp_df$Time[j]),
+                                                                   units = "hours"))
         }
     }
     
@@ -69,7 +91,7 @@ for(i in unique(igotu_data_all$ID)) {
         # remove dropped points
         filter(Keep == "YES") %>% 
         # only take columns of interest
-        select(ID, Latitude, Longitude, Date, Time, DateTime, Party_ID, Harvest, Elapsed_Time)
+        select(ID, Latitude, Longitude, Date, Time, DateTime, Party_ID, Harvest, Elapsed_Time, Elapsed_Time_Sunrise, Elapsed_Time_Sunset)
     
     write.csv(temp_df, paste0("Data/Hunting/igotu_cleaned/", i, ".csv"), row.names = F)
     
