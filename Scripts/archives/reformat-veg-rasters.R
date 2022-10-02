b7fe90ed8d29ed95094f98b9777d69a05a2a4cb3
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2022-10-02 10:52:32 -0700

    Resample/project viewshed as well

--- Scripts/archives/reformat-veg-rasters.R
+++ Scripts/archives/reformat-veg-rasters.R
@@ -4,6 +4,7 @@ library(raster)
 chap <- raster("Data/Spatial data/veg_120/chap_120m")
 wood <- raster("Data/Spatial data/veg_120/wood_120m")
 grass <- raster("Data/Spatial data/veg_120/grass_120m")
+view <- raster("Data/Spatial data/view/view_for_kg.tif")
 
 # get in same resolution
 blm_dist <- raster("Data/Spatial data/Cleaned rasters/blm_dist.tif")
@@ -12,17 +13,19 @@ blm_dist <- raster("Data/Spatial data/Cleaned rasters/blm_dist.tif")
 raster::extent(chap) <- blm_dist
 raster::extent(wood) <- blm_dist
 raster::extent(grass) <- blm_dist
+raster::extent(view) <- blm_dist
 
 # make resolution the same
 chap <- raster::resample(chap, blm_dist, method = 'bilinear') 
 wood <- raster::resample(wood, blm_dist, method = 'bilinear') 
 grass <- raster::resample(grass, blm_dist, method = 'bilinear') 
+view <- raster::resample(view, blm_dist, method = 'bilinear')
 
 # test stack
-test_stack <- raster::stack(blm_dist, chap, wood, grass)
+test_stack <- raster::stack(blm_dist, chap, wood, grass, view)
 
 # export
 raster::writeRaster(chap, 'Data/Spatial data/Cleaned rasters/chap_120m.tif', format="GTiff", overwrite=TRUE, options=c("INTERLEAVE=BAND","COMPRESS=LZW"))
 raster::writeRaster(wood, 'Data/Spatial data/Cleaned rasters/wood_120m.tif', format="GTiff", overwrite=TRUE, options=c("INTERLEAVE=BAND","COMPRESS=LZW"))
 raster::writeRaster(grass, 'Data/Spatial data/Cleaned rasters/grass_120m.tif', format="GTiff", overwrite=TRUE, options=c("INTERLEAVE=BAND","COMPRESS=LZW"))
-
+raster::writeRaster(view, 'Data/Spatial data/Cleaned rasters/view_for_kg_proj.tif', format="GTiff", overwrite=TRUE, options=c("INTERLEAVE=BAND","COMPRESS=LZW"))
