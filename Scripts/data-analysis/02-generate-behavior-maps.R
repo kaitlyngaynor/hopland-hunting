9f00d7931de013f7892bc106f2cac1a8c5f84e34
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2022-10-01 21:56:19 -0700

    Split map plots out of raster generation script

--- Scripts/data-analysis/02-generate-behavior-maps.R
+++ Scripts/data-analysis/02-generate-behavior-maps.R
@@ -5,7 +5,6 @@
 library(sf)
 library(raster)
 library(SpatialKDE)
-library(viridis)
 library(dplyr)
 
 # Bring in data with model results
@@ -49,76 +48,6 @@ stationary_dens_weighted <- SpatialKDE::kde(data_hmm_sf, band_width = 400, grid 
                                          weights = data_hmm_sf$Stationary_Prob)
 driving_dens_weighted <- SpatialKDE::kde(data_hmm_sf, band_width = 400, grid = reference,
                                             weights = data_hmm_sf$Driving_Prob)
-
-
-# Comparison plots --------------------------------------------------------
-
-# Weighted vs unweighted for each behavioral state
-
-# Stationary
-par(mfrow=c(1,2))
-plot(walking_dens_unweighted, 
-     col = viridis(1e3), 
-     zlim=c(0,400),
-     main = "Walking Unweighted")
-plot(walking_dens_weighted, 
-     col = viridis(1e3), 
-     zlim=c(0,400),
-     main = "Walking Weighted")
-
-# Walking
-par(mfrow=c(1,2))
-plot(stationary_dens_unweighted, 
-     col = viridis(1e3), 
-     zlim=c(0,400),
-     main = "Stationary Unweighted")
-plot(stationary_dens_weighted, 
-     col = viridis(1e3), 
-     zlim=c(0,400),
-     main = "Stationary Weighted")
-
-# Driving
-par(mfrow=c(1,2))
-plot(driving_dens_unweighted, 
-     col = viridis(1e3), 
-     zlim=c(0,400),
-     main = "Driving Unweighted")
-plot(driving_dens_weighted, 
-     col = viridis(1e3), 
-     zlim=c(0,400),
-     main = "Driving Weighted")
-
-# All 3 unweighted states
-par(mfrow=c(1,3))
-plot(stationary_dens_unweighted, 
-     col = viridis(1e3), 
-     zlim=c(0,400),
-     main = "Stationary (Unweighted)")
-plot(walking_dens_unweighted, 
-     col = viridis(1e3), 
-     zlim=c(0,400),
-     main = "Walking (Unweighted)")
-plot(driving_dens_unweighted, 
-     col = viridis(1e3), 
-     zlim=c(0,400),
-     main = "Driving (Unweighted)")
-
-# All 3 weighted states
-par(mfrow=c(1,3))
-plot(stationary_dens_weighted, 
-     col = viridis(1e3), 
-     zlim=c(0,400),
-     main = "Stationary (Weighted)")
-plot(walking_dens_weighted, 
-     col = viridis(1e3), 
-     zlim=c(0,400),
-     main = "Walking (Weighted)")
-plot(driving_dens_weighted, 
-     col = viridis(1e3), 
-     zlim=c(0,400),
-     main = "Driving (Weighted)")
-
-# All 3 weighted states
 
 
 # Export rasters ----------------------------------------------------------
