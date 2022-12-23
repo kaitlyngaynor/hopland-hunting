f578e5a9f5bb316014c2a53174f1695376837fd3
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2022-12-23 11:11:14 -0800

    Run separate RSFs for the two stationary states

--- Scripts/data-analysis/06-rsf-by-behavior.R
+++ Scripts/data-analysis/06-rsf-by-behavior.R
@@ -12,14 +12,14 @@ head(used)
 
 # Join into single dataframe and scale covariates
 used_avail <- dplyr::bind_rows(used, available) %>% 
-    dplyr::select(Ruggedness, Viewshed, Habitat, Road_Distance, Chaparral_120m, Woodland_120m, 
+    dplyr::select(Ruggedness, Viewshed, Road_Distance, Chaparral_120m, Woodland_120m, HQ_Distance,
                   Used, state, state_2stationary) %>% 
     dplyr::mutate(Ruggedness_scale = scale(Ruggedness),
                   Viewshed_scale = scale(Viewshed),
                   Road_Distance_scale = scale(Road_Distance),
                   Chaparral_120m_scale = scale(Chaparral_120m),
                   Woodland_120m_scale = scale(Woodland_120m),
-                  Habitat = as.factor(Habitat))
+                  HQ_Distance_scale = scale(HQ_Distance))
 
 # Split used by behavior (retain all available)
 used_avail_stationary <- used_avail %>% 
@@ -28,29 +28,41 @@ used_avail_walking <- used_avail %>%
     dplyr::filter((state != "Stationary" & state != "Driving") | Used == 0)
 used_avail_driving <- used_avail %>% 
     dplyr::filter((state != "Stationary" & state != "Walking") | Used == 0)
+used_avail_stationary_road <- used_avail %>% 
+    dplyr::filter((state_2stationary != "Driving" & state_2stationary != "Walking" & state_2stationary != "Stationary_offroad") | Used == 0)
+used_avail_stationary_offroad <- used_avail %>% 
+    dplyr::filter((state_2stationary != "Driving" & state_2stationary != "Walking" & state_2stationary != "Stationary_road") | Used == 0)
 
 # Run separate RSF for each behavioral state
 # Explored dredging full model - the full model is best
-fit_stat <- glm(Used ~ Ruggedness_scale + Viewshed_scale + Road_Distance_scale + Chaparral_120m_scale + Woodland_120m_scale,
+fit_stat <- glm(Used ~ Ruggedness_scale + Viewshed_scale + Road_Distance_scale + Chaparral_120m_scale + Woodland_120m_scale + HQ_Distance_scale,
                     data = used_avail_stationary,
                     family = binomial) 
-fit_walk <- glm(Used ~ Ruggedness_scale + Viewshed_scale + Road_Distance_scale + Chaparral_120m_scale + Woodland_120m_scale,
+fit_stat_road <- glm(Used ~ Ruggedness_scale + Viewshed_scale + Chaparral_120m_scale + Woodland_120m_scale + HQ_Distance_scale,
+                data = used_avail_stationary_road,
+                family = binomial) # can't use road in this, doesn't converge
+fit_stat_offroad <- glm(Used ~ Ruggedness_scale + Viewshed_scale + Road_Distance_scale + Chaparral_120m_scale + Woodland_120m_scale + HQ_Distance_scale,
+                data = used_avail_stationary_offroad,
+                family = binomial) 
+fit_walk <- glm(Used ~ Ruggedness_scale + Viewshed_scale + Road_Distance_scale + Chaparral_120m_scale + Woodland_120m_scale + HQ_Distance_scale,
                     data = used_avail_walking,
                     family = binomial) 
-fit_driv <- glm(Used ~ Ruggedness_scale + Viewshed_scale + Road_Distance_scale + Chaparral_120m_scale + Woodland_120m_scale,
+fit_driv <- glm(Used ~ Ruggedness_scale + Viewshed_scale + Road_Distance_scale + Chaparral_120m_scale + Woodland_120m_scale + HQ_Distance_scale,
                     data = used_avail_driving,
                     family = binomial) 
 # getting message that fitted probabilities equal to 0 or 1 occurred - likely because driving is SO tied to roads. model not converging
-fit_driv <- glm(Used ~ Ruggedness_scale + Viewshed_scale + Chaparral_120m_scale + Woodland_120m_scale,
+fit_driv <- glm(Used ~ Ruggedness_scale + Viewshed_scale + Chaparral_120m_scale + Woodland_120m_scale + HQ_Distance_scale,
                 data = used_avail_driving,
-                family = binomial) # version without road distance in it - but doesn't make much sense
+                family = binomial) # version without road distance in it (but seems inappropriate)
 
 # Run one RSF for all combined
-fit_all <- glm(Used ~ Ruggedness_scale + Viewshed_scale + Road_Distance_scale + Chaparral_120m_scale + Woodland_120m_scale,
+fit_all <- glm(Used ~ Ruggedness_scale + Viewshed_scale + Road_Distance_scale + Chaparral_120m_scale + Woodland_120m_scale + HQ_Distance_scale,
                 data = used_avail,
                 family = binomial) 
 
 summary(fit_stat)
+summary(fit_stat_road)
+summary(fit_stat_offroad)
 summary(fit_walk)
 summary(fit_driv)
 summary(fit_all)
@@ -59,6 +71,6 @@ summary(fit_all)
 jtools::plot_summs(fit_stat, fit_walk)
 jtools::plot_summs(fit_stat, fit_walk, fit_driv,
                    model.names = c("Stationary", "Walking", "Driving"))
-jtools::plot_summs(fit_stat, fit_walk, fit_all,
-                   model.names = c("Stationary", "Walking", "All states"))
+jtools::plot_summs(fit_stat_offroad, fit_stat_road, fit_walk, fit_driv, fit_all,
+                   model.names = c("Stationary (Offroad only)", "Stationary (Road)", "Walking", "Driving", "All states"))
 
