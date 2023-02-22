cd9b8b4562bac761e9486d5badb13a11da3bfede
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2023-02-22 12:33:03 -0800

    Explore harvest rate and track recovery by single vs multiday hunts

--- Scripts/data-analysis/calculate-harvest-rate-all.R
+++ Scripts/data-analysis/calculate-harvest-rate-all.R
@@ -1,7 +1,10 @@
 library(dplyr)
+library(tidyr)
+library(ggplot2)
 
-metadata1 <- read.csv("Data/Hunting/igotu_metadata_times_cleaned_28Nov2022.csv")
-metadata2 <- read.csv("Data/Hunting/igotu_metadata_2019_2020_29Jan2023.csv")
+metadata1 <- read.csv("Data/Hunting/igotu_metadata_times_cleaned_28Nov2022.csv") %>% 
+    mutate(Hunt_type = "Single_day")
+metadata2 <- read.csv("Data/Hunting/igotu_metadata_2019_2022_29Jan2023.csv")
 
 metadata <- bind_rows(metadata1, metadata2)
 
@@ -10,23 +13,53 @@ hunters <- metadata %>%
 
 count(hunters, Harvest)
 
-# 45 yes, 350 no, 15 unknown
-# But we know that of the 15 unknown, 2 were yes, and 13 were no
-
-# So, final count:
-
-# 47 YES HARVEST
-# 363 NO HARVEST
+# 89 YES HARVEST
+# 497 NO HARVEST
 
 # How many to use?
 count(hunters, Good_track)
-# 410 total tracks
-# 386 good tracks, 24 tracks not good
+# 586 total tracks
+# 462 good tracks, 124 tracks not good
+
+# Figure of tracks by year (usable vs not)
+count(hunters, Good_track, Year) %>% 
+    #pivot_wider(names_from = Good_track, values_from = n) %>% 
+    ggplot(aes(x = Year, y = n, fill = Good_track)) +
+    geom_bar(stat = "identity") +
+    theme_bw() +
+    scale_fill_manual(values = c("#FE7C00", "#4169E1")) +
+    ylab("Number of Hunters")
+
+# Figure of single vs. multi-day hunters
+hunters %>% 
+    filter(Good_track == "Y") %>% 
+    count(Year, Hunt_type) %>% 
+    ggplot(aes(x = Year, y = n, fill = Hunt_type)) +
+    geom_bar(stat = "identity") +
+    theme_bw() +
+    scale_fill_manual(values = c("#4169E1", "#808080")) +
+    ylab("Number of Hunter Tracks")
 
 # How many successful/unsuccessful used?
 hunters %>% 
-    group_by(Good_track) %>%
-    count(Harvest)
+    count(Good_track, Harvest) %>% 
+    ggplot(aes(x = Good_track, y = n, fill = Harvest)) +
+    geom_bar(stat = "identity") +
+    theme_bw() +
+    scale_fill_manual(values = c("#FE7C00", "#4169E1")) +
+    ylab("Number of Hunters")
 
-# 386 good tracks = 41 harvest, 330 no harvest, 15 unknown
-# 24 bad tracks = 4 harvest, 20 no harvest
+# 462 good tracks = 62 harvest, 400 no harvest (13%)
+# 124 bad tracks = 27 harvest, 97 no harvest (22%)
+
+# How many successful/unsuccessful by hunt type?
+hunters %>% 
+    count(Hunt_type, Harvest) %>% 
+    ggplot(aes(x = Hunt_type, y = n, fill = Harvest)) +
+    geom_bar(stat = "identity") +
+    theme_bw() +
+    scale_fill_manual(values = c("#4169E1", "#808080")) +
+    ylab("Number of Hunters")
+
+# 75 multiday hunters = 28 harvest, 47 no harvest (37%)
+# 511 single day hunters = 61 harvest, 450 no harvest (12%)
