e915b2080537781feeb14d50c0e82e27c88fae95
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2023-03-10 08:41:51 -0800

    Export histogram of distance-to-road for stationary points

--- Scripts/data-analysis/02-closer-look-at-behavior.R
+++ Scripts/data-analysis/02-closer-look-at-behavior.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(ggplot2)
+library(cowplot)
 
 data_hmm <- read.csv("Results/hmm-top-model-2023-03-04.csv")
 head(data_hmm)
@@ -9,7 +10,24 @@ head(data_hmm)
 
 # Split stationary on road vs. stationary off road
 stationary <- data_hmm %>% dplyr::filter(state == "Stationary")
-hist(stationary$Road_Distance) # if you look closely, big peak between 5-10 meters, then drops off
+(hist_all <- ggplot(stationary, aes(Road_Distance)) +
+    geom_histogram() +
+    theme_bw() +
+    xlab("Distance to Road (meters)") +
+    ylab("Frequency of Stationary Points"))
+
+# if you look closely, big peak between 5-10 meters, then drops off
+(hist_50 <- stationary %>% 
+    dplyr::filter(Road_Distance < 50) %>% 
+    ggplot(aes(Road_Distance)) +
+    geom_histogram() +
+    theme_bw() +
+    xlab("Distance to Road (meters)") +
+    ylab("Frequency of Stationary Points"))
+
+plot_grid(hist_all, hist_50, labels = "AUTO")
+ggsave("Figures/stationary-road-histogram.pdf", width = 6, height = 3)
+
 # Consider within 10 meters to be "on road"
 
 # Create new column with 4 categories
