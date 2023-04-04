7ecbdaaa1e174336928e4eb88f2a1b2f78a33129
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2023-04-03 19:05:59 -0700

    Update to use solar time instead of time since sunrise

--- Scripts/data-analysis/07-explore-harvest-times.R
+++ Scripts/data-analysis/07-explore-harvest-times.R
@@ -1,58 +1,71 @@
 library(dplyr)
 library(ggplot2)
+library(overlap)
+library(kSamples)
+`%notin%` <- Negate(`%in%`)
 
 success <- read.csv("Results/hunter_cluster_success_long.csv") %>%
-    tidyr::pivot_wider(names_from = "State_4state", values_from = "Percentage") %>%
-    dplyr::filter(Harvest != "unknown") %>% 
-    dplyr::mutate(Harvest01 = ifelse(Harvest == "N",0,1))
+    tidyr::pivot_wider(names_from = "State_4state", values_from = "Percentage") %>% 
+    dplyr::filter(Harvest == "Y") %>% 
+    dplyr::select(ID, Cluster4)
 
 # Bring in metadata
 metadata1 <- read.csv("Data/Hunting/igotu_metadata_times_cleaned_28Nov2022.csv")
 metadata2 <- read.csv("Data/Hunting/igotu_metadata_2019_2022_29Jan2023.csv")
-metadata <- bind_rows(metadata1, metadata2)
-success <- left_join(success, metadata)
+metadata <- bind_rows(metadata1, metadata2) %>% 
+    dplyr::select(ID, Date, Harvest_time)
+success <- left_join(success, metadata)  %>%
+    dplyr::filter(ID %notin% c("082419_06", "082419_13")) # missing harvest times
 
 # Calculate time from sunrise (06:00) to harvest
 success$Sunrise_elapsed_min <- as.numeric(difftime(as.POSIXct(success$Harvest_time, format = "%H:%M"), 
                                                    as.POSIXct("06:00", format = "%H:%M"),
                                                    units = "mins"))
-success$Sunrise_elapsed_min_scale <- scale(success$Sunrise_elapsed_min)
+
+# Scale times
+success$Date <- as.POSIXct(success$Date, format = "%m/%d/%y")
+success$Time_HM <- lubridate::hm(success$Harvest_time)
+success$Time_Decimal <- success$Time_HM$hour + success$Time_HM$minute/60
+success$Time_Scaled <- success$Time_Decimal / 24
+success$Time_Radians <- success$Time_Scaled * 2 * pi
+coords <- matrix(c(-123.079, 39.0013), nrow=1) # note it is c(longitude, latitude)
+Coords <- sp::SpatialPoints(coords,
+                            proj4string=sp::CRS("+proj=longlat +datum=WGS84"))
+success$Time_Sun <- overlap::sunTime(success$Time_Radians, success$Date, Coords)
 
 
 set.seed(678)
 success %>% 
-    dplyr::filter(Harvest == "Y") %>%
-    ggplot(aes(x = Sunrise_elapsed_min/60, y = Cluster4, col = Cluster4)) +
+    ggplot(aes(x = Time_Sun, y = Cluster4, col = Cluster4)) +
     geom_point(size = 3, alpha = 0.75, position = position_jitter(w = 0.15, h = 0.15)) +
     theme_bw() +
     ylab("Hunting Mode") +
-    xlab("Time From Sunrise (hours)") +
+    xlab("Time of Day") +
     theme(legend.position = "none") +
     scale_color_manual(values = c("#1b9e77", "#d95f02", "#7570b3"))
 
 # Density plot with jittered points
 success %>% 
-    dplyr::filter(Harvest == "Y") %>%
-    ggplot(aes(x = Sunrise_elapsed_min/60, fill = Cluster4)) +
+    ggplot(aes(x = Time_Sun, fill = Cluster4)) +
     geom_density(alpha = 0.75) +
     theme_bw() +
     facet_wrap(~Cluster4, ncol = 1) +
     ylab("Density") +
-    xlab("Time From Sunrise (hours)") +
+    xlab("Time of Day") +
     theme(legend.position = "none") +
-    geom_jitter(data = success, aes(x = Sunrise_elapsed_min/60, y = -0.0125, col = Cluster4), width = 0.25, height = 0, alpha = 0.5, size = 2) +
+    geom_jitter(data = success, aes(x = Time_Sun, y = -0.0125, col = Cluster4), width = 0.25, height = 0, alpha = 0.5, size = 2) +
     scale_fill_manual(values = c("#1b9e77", "#d95f02", "#7570b3")) +
-    scale_color_manual(values = c("#1b9e77", "#d95f02", "#7570b3"))
+    scale_color_manual(values = c("#1b9e77", "#d95f02", "#7570b3")) +
+    scale_x_continuous(breaks = c(pi/2, pi, (3*pi)/2),
+                       labels = c("Sunrise", "Noon", "Sunset"))
 ggsave("Figures/time-of-harvest.pdf", width = 3, height = 5)
 
 # See if time of day varies by hunting mode - Anderson-Darling test
 
-library(kSamples)
-
-coursing <- dplyr::filter(success, Cluster4 == "Coursing", Harvest == "Y")
-stalking <- dplyr::filter(success, Cluster4 == "Stalking", Harvest == "Y")
-sitandwait <- dplyr::filter(success, Cluster4 == "Sit-and-wait", Harvest == "Y")
+coursing <- dplyr::filter(success, Cluster4 == "Coursing")
+stalking <- dplyr::filter(success, Cluster4 == "Stalking")
+sitandwait <- dplyr::filter(success, Cluster4 == "Sit-and-wait")
 
 set.seed(12)
-ad.test(coursing$Sunrise_elapsed_min, stalking$Sunrise_elapsed_min, sitandwait$Sunrise_elapsed_min,
+ad.test(coursing$Time_Sun, stalking$Time_Sun, sitandwait$Time_Sun,
         method = "exact", dist = FALSE, Nsim = 1000)
