e9c4e494bf762fa5eb149a753f783f789f9f283e
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2023-04-03 19:27:01 -0700

    Replace base R plot with ggplot

--- Scripts/results-exploration/plot-buck-activity.R
+++ Scripts/results-exploration/plot-buck-activity.R
@@ -54,20 +54,16 @@ buck_records$Time_Sun <- overlap::sunTime(buck_records$Time_Radians, buck_record
 
 # Plot diel activity
 bwA <- getBandWidth(buck_records$Time_Sun, kmax = 3)
+diel_data <- data.frame(matrix(ncol = 0, nrow = 128))
+diel_data$xxRad <- seq(0, 2 * pi, length = 128)
+diel_data$densA <- densityFit(buck_records$Time_Sun, diel_data$xxRad, bwA)/(24/(2 * pi))
 
-xsc <- 24/(2 * pi)
-xxRad <- seq(0, 2 * pi, length = 128)
-xx <- xxRad * xsc
-densA <- densityFit(buck_records$Time_Sun, xxRad, bwA)/xsc
-
-plot(0, 0, type = "n", ylim = c(0, 0.08), xlim = c(6,18), 
-     xlab = "Time", 
-     ylab = "Activity", 
-     xaxt = "n")
-
-axis(1, at = c(6, 12, 18), labels = c("Sunrise", "Noon", "Sunset"))
-polygon(c(c(2 * pi, 0), xx,24), c(0, 0, densA,0), border = NA, 
-        col = "lightgrey")
-lines(xx, densA, lty = 1, col = "black")
-
+ggplot(diel_data, aes(x = xxRad, y = densA)) +
+    geom_line() +
+    geom_ribbon(aes(x = xxRad, ymax = densA), ymin=0, alpha=0.5) +
+    theme_bw() +
+    scale_x_continuous(limits = c(pi/2, (3*pi)/2),
+                       breaks = c(pi/2, pi, (3*pi)/2),
+                       labels = c("Sunrise", "Noon", "Sunset")) +
+    ylim(c(0, 0.06))
 
