863ebbc31f1b53f1c6b484c0bca4b08bc7a60796
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2023-06-22 12:58:49 -0700

    Make marginal effects plots

--- Scripts/data-analysis/06-rsf-by-behavior.R
+++ Scripts/data-analysis/06-rsf-by-behavior.R
@@ -36,6 +36,20 @@ used_avail <- dplyr::bind_rows(used, available_100) %>%
                   Woodland_120m_scale = scale(Woodland_120m),
                   HQ_Distance_scale = scale(HQ_Distance))
 
+# Save scaling parameters
+Ruggedness_scale_list <- list(scale = attr(used_avail$Ruggedness_scale, "scaled:scale"),
+                              center = attr(used_avail$Ruggedness_scale, "scaled:center"))
+Viewshed_scale_list <- list(scale = attr(used_avail$Viewshed_scale, "scaled:scale"),
+                              center = attr(used_avail$Viewshed_scale, "scaled:center"))
+Road_Distance_scale_list <- list(scale = attr(used_avail$Road_Distance_scale, "scaled:scale"),
+                              center = attr(used_avail$Road_Distance_scale, "scaled:center"))
+Chaparral_120m_scale_list <- list(scale = attr(used_avail$Chaparral_120m_scale, "scaled:scale"),
+                              center = attr(used_avail$Chaparral_120m_scale, "scaled:center"))
+Woodland_120m_scale_list <- list(scale = attr(used_avail$Woodland_120m_scale, "scaled:scale"),
+                              center = attr(used_avail$Woodland_120m_scale, "scaled:center"))
+HQ_Distance_scale_list <- list(scale = attr(used_avail$HQ_Distance_scale, "scaled:scale"),
+                              center = attr(used_avail$HQ_Distance_scale, "scaled:center"))
+
 # bring in clusters
 clusters <- read.csv("Results/hunter_cluster_success_long.csv") %>%
     tidyr::pivot_wider(names_from = "State_4state", values_from = "Percentage") %>% 
@@ -184,4 +198,281 @@ all_rsf_results <- dplyr::bind_rows(coursing_success_results, coursing_unsuccess
                                     stalking_success_results, stalking_unsuccess_results,
                                     sitandwait_success_results, sitandwait_unsuccess_results)
 write.csv(all_rsf_results, "Results/rsf-results-by-mode-success-30min.csv", row.names = F)
-    
+
+library(ggplot2)
+library(effects)
+library(egg)
+
+
+# Ruggedness RSF plots ----------------------------------------------------
+
+# ruggedness
+coursing_success_rugged <- predictorEffect("Ruggedness_scale", fit_coursing_success) %>% 
+    as_tibble() %>% 
+    mutate(Ruggedness_unscaled = Ruggedness_scale * Ruggedness_scale_list$scale + Ruggedness_scale_list$center,
+           Success = "Successful",
+           Mode = "Coursing") 
+coursing_unsuccess_rugged <- predictorEffect("Ruggedness_scale", fit_coursing_unsuccess) %>% 
+    as_tibble() %>% 
+    mutate(Ruggedness_unscaled = Ruggedness_scale * Ruggedness_scale_list$scale + Ruggedness_scale_list$center,
+           Success = "Unsuccessful",
+           Mode = "Coursing") 
+stalking_success_rugged <- predictorEffect("Ruggedness_scale", fit_stalking_success) %>% 
+    as_tibble() %>% 
+    mutate(Ruggedness_unscaled = Ruggedness_scale * Ruggedness_scale_list$scale + Ruggedness_scale_list$center,
+           Success = "Successful",
+           Mode = "Stalking") 
+stalking_unsuccess_rugged <- predictorEffect("Ruggedness_scale", fit_stalking_unsuccess) %>% 
+    as_tibble() %>% 
+    mutate(Ruggedness_unscaled = Ruggedness_scale * Ruggedness_scale_list$scale + Ruggedness_scale_list$center,
+           Success = "Unsuccessful",
+           Mode = "Stalking") 
+sitandwait_success_rugged <- predictorEffect("Ruggedness_scale", fit_sitandwait_success) %>% 
+    as_tibble() %>% 
+    mutate(Ruggedness_unscaled = Ruggedness_scale * Ruggedness_scale_list$scale + Ruggedness_scale_list$center,
+           Success = "Successful",
+           Mode = "Sit-and-Wait") 
+sitandwait_unsuccess_rugged <- predictorEffect("Ruggedness_scale", fit_sitandwait_unsuccess) %>% 
+    as_tibble() %>% 
+    mutate(Ruggedness_unscaled = Ruggedness_scale * Ruggedness_scale_list$scale + Ruggedness_scale_list$center,
+           Success = "Unsuccessful",
+           Mode = "Sit-and-Wait") 
+rugged_predict <- dplyr::bind_rows(coursing_success_rugged,
+                                   coursing_unsuccess_rugged,
+                                   stalking_success_rugged,
+                                   stalking_unsuccess_rugged,
+                                   sitandwait_success_rugged,
+                                   sitandwait_unsuccess_rugged)
+
+ggplot(rugged_predict, aes(x = Ruggedness_unscaled, y = fit))+
+    geom_line(aes(col = Success)) +
+    geom_ribbon(aes(ymin = lower, ymax = upper, fill = Success), alpha = 0.2) +
+    labs(y = "Probability of Use", x = "Ruggedness") +
+    #scale_fill_manual(values = c("#d8b365", "#969696", "#4d938a")) +
+    #scale_color_manual(values = c("#d8b365", "#969696", "#4d938a")) +
+    theme_bw() +
+    facet_wrap(~ Mode)
+
+# Road RSF plots ----------------------------------------------------
+
+coursing_success_road <- predictorEffect("Road_Distance_scale", fit_coursing_success) %>% 
+    as_tibble() %>% 
+    mutate(Road_Distance_unscaled = Road_Distance_scale * Road_Distance_scale_list$scale + Road_Distance_scale_list$center,
+           Success = "Successful",
+           Mode = "Coursing") 
+coursing_unsuccess_road <- predictorEffect("Road_Distance_scale", fit_coursing_unsuccess) %>% 
+    as_tibble() %>% 
+    mutate(Road_Distance_unscaled = Road_Distance_scale * Road_Distance_scale_list$scale + Road_Distance_scale_list$center,
+           Success = "Unsuccessful",
+           Mode = "Coursing") 
+stalking_success_road <- predictorEffect("Road_Distance_scale", fit_stalking_success) %>% 
+    as_tibble() %>% 
+    mutate(Road_Distance_unscaled = Road_Distance_scale * Road_Distance_scale_list$scale + Road_Distance_scale_list$center,
+           Success = "Successful",
+           Mode = "Stalking") 
+stalking_unsuccess_road <- predictorEffect("Road_Distance_scale", fit_stalking_unsuccess) %>% 
+    as_tibble() %>% 
+    mutate(Road_Distance_unscaled = Road_Distance_scale * Road_Distance_scale_list$scale + Road_Distance_scale_list$center,
+           Success = "Unsuccessful",
+           Mode = "Stalking") 
+sitandwait_success_road <- predictorEffect("Road_Distance_scale", fit_sitandwait_success) %>% 
+    as_tibble() %>% 
+    mutate(Road_Distance_unscaled = Road_Distance_scale * Road_Distance_scale_list$scale + Road_Distance_scale_list$center,
+           Success = "Successful",
+           Mode = "Sit-and-Wait") 
+sitandwait_unsuccess_road <- predictorEffect("Road_Distance_scale", fit_sitandwait_unsuccess) %>% 
+    as_tibble() %>% 
+    mutate(Road_Distance_unscaled = Road_Distance_scale * Road_Distance_scale_list$scale + Road_Distance_scale_list$center,
+           Success = "Unsuccessful",
+           Mode = "Sit-and-Wait") 
+road_predict <- dplyr::bind_rows(coursing_success_road,
+                                   coursing_unsuccess_road,
+                                   stalking_success_road,
+                                   stalking_unsuccess_road,
+                                   sitandwait_success_road,
+                                   sitandwait_unsuccess_road)
+
+ggplot(road_predict, aes(x = Road_Distance_unscaled, y = fit))+
+    geom_line(aes(col = Success)) +
+    geom_ribbon(aes(ymin = lower, ymax = upper, fill = Success), alpha = 0.2) +
+    labs(y = "Probability of Use", x = "Road Distance") +
+    #scale_fill_manual(values = c("#d8b365", "#969696", "#4d938a")) +
+    #scale_color_manual(values = c("#d8b365", "#969696", "#4d938a")) +
+    theme_bw() +
+    facet_wrap(~ Mode)
+
+
+# Chaparral RSF plots ----------------------------------------------------
+
+coursing_success_chaparral <- predictorEffect("Chaparral_120m_scale", fit_coursing_success) %>% 
+    as_tibble() %>% 
+    mutate(Chaparral_120m_unscaled = Chaparral_120m_scale * Chaparral_120m_scale_list$scale + Chaparral_120m_scale_list$center,
+           Success = "Successful",
+           Mode = "Coursing") 
+coursing_unsuccess_chaparral <- predictorEffect("Chaparral_120m_scale", fit_coursing_unsuccess) %>% 
+    as_tibble() %>% 
+    mutate(Chaparral_120m_unscaled = Chaparral_120m_scale * Chaparral_120m_scale_list$scale + Chaparral_120m_scale_list$center,
+           Success = "Unsuccessful",
+           Mode = "Coursing") 
+stalking_success_chaparral <- predictorEffect("Chaparral_120m_scale", fit_stalking_success) %>% 
+    as_tibble() %>% 
+    mutate(Chaparral_120m_unscaled = Chaparral_120m_scale * Chaparral_120m_scale_list$scale + Chaparral_120m_scale_list$center,
+           Success = "Successful",
+           Mode = "Stalking") 
+stalking_unsuccess_chaparral <- predictorEffect("Chaparral_120m_scale", fit_stalking_unsuccess) %>% 
+    as_tibble() %>% 
+    mutate(Chaparral_120m_unscaled = Chaparral_120m_scale * Chaparral_120m_scale_list$scale + Chaparral_120m_scale_list$center,
+           Success = "Unsuccessful",
+           Mode = "Stalking") 
+sitandwait_success_chaparral <- predictorEffect("Chaparral_120m_scale", fit_sitandwait_success) %>% 
+    as_tibble() %>% 
+    mutate(Chaparral_120m_unscaled = Chaparral_120m_scale * Chaparral_120m_scale_list$scale + Chaparral_120m_scale_list$center,
+           Success = "Successful",
+           Mode = "Sit-and-Wait") 
+sitandwait_unsuccess_chaparral <- predictorEffect("Chaparral_120m_scale", fit_sitandwait_unsuccess) %>% 
+    as_tibble() %>% 
+    mutate(Chaparral_120m_unscaled = Chaparral_120m_scale * Chaparral_120m_scale_list$scale + Chaparral_120m_scale_list$center,
+           Success = "Unsuccessful",
+           Mode = "Sit-and-Wait") 
+chaparral_predict <- dplyr::bind_rows(coursing_success_chaparral,
+                                 coursing_unsuccess_chaparral,
+                                 stalking_success_chaparral,
+                                 stalking_unsuccess_chaparral,
+                                 sitandwait_success_chaparral,
+                                 sitandwait_unsuccess_chaparral)
+
+ggplot(chaparral_predict, aes(x = Chaparral_120m_unscaled, y = fit))+
+    geom_line(aes(col = Success)) +
+    geom_ribbon(aes(ymin = lower, ymax = upper, fill = Success), alpha = 0.2) +
+    labs(y = "Probability of Use", x = "Chaparral Density") +
+    #scale_fill_manual(values = c("#d8b365", "#969696", "#4d938a")) +
+    #scale_color_manual(values = c("#d8b365", "#969696", "#4d938a")) +
+    theme_bw() +
+    facet_wrap(~ Mode)
+
+# Woodland RSF plots ----------------------------------------------------
+
+coursing_success_woodland <- predictorEffect("Woodland_120m_scale", fit_coursing_success) %>% 
+    as_tibble() %>% 
+    mutate(Woodland_120m_unscaled = Woodland_120m_scale * Woodland_120m_scale_list$scale + Woodland_120m_scale_list$center,
+           Success = "Successful",
+           Mode = "Coursing") 
+coursing_unsuccess_woodland <- predictorEffect("Woodland_120m_scale", fit_coursing_unsuccess) %>% 
+    as_tibble() %>% 
+    mutate(Woodland_120m_unscaled = Woodland_120m_scale * Woodland_120m_scale_list$scale + Woodland_120m_scale_list$center,
+           Success = "Unsuccessful",
+           Mode = "Coursing") 
+stalking_success_woodland <- predictorEffect("Woodland_120m_scale", fit_stalking_success) %>% 
+    as_tibble() %>% 
+    mutate(Woodland_120m_unscaled = Woodland_120m_scale * Woodland_120m_scale_list$scale + Woodland_120m_scale_list$center,
+           Success = "Successful",
+           Mode = "Stalking") 
+stalking_unsuccess_woodland <- predictorEffect("Woodland_120m_scale", fit_stalking_unsuccess) %>% 
+    as_tibble() %>% 
+    mutate(Woodland_120m_unscaled = Woodland_120m_scale * Woodland_120m_scale_list$scale + Woodland_120m_scale_list$center,
+           Success = "Unsuccessful",
+           Mode = "Stalking") 
+sitandwait_success_woodland <- predictorEffect("Woodland_120m_scale", fit_sitandwait_success) %>% 
+    as_tibble() %>% 
+    mutate(Woodland_120m_unscaled = Woodland_120m_scale * Woodland_120m_scale_list$scale + Woodland_120m_scale_list$center,
+           Success = "Successful",
+           Mode = "Sit-and-Wait") 
+sitandwait_unsuccess_woodland <- predictorEffect("Woodland_120m_scale", fit_sitandwait_unsuccess) %>% 
+    as_tibble() %>% 
+    mutate(Woodland_120m_unscaled = Woodland_120m_scale * Woodland_120m_scale_list$scale + Woodland_120m_scale_list$center,
+           Success = "Unsuccessful",
+           Mode = "Sit-and-Wait") 
+woodland_predict <- dplyr::bind_rows(coursing_success_woodland,
+                                      coursing_unsuccess_woodland,
+                                      stalking_success_woodland,
+                                      stalking_unsuccess_woodland,
+                                      sitandwait_success_woodland,
+                                      sitandwait_unsuccess_woodland)
+
+ggplot(woodland_predict, aes(x = Woodland_120m_unscaled, y = fit))+
+    geom_line(aes(col = Success)) +
+    geom_ribbon(aes(ymin = lower, ymax = upper, fill = Success), alpha = 0.2) +
+    labs(y = "Probability of Use", x = "Woodland Density") +
+    #scale_fill_manual(values = c("#d8b365", "#969696", "#4d938a")) +
+    #scale_color_manual(values = c("#d8b365", "#969696", "#4d938a")) +
+    theme_bw() +
+    facet_wrap(~ Mode)
+
+
+# Viewshed RSF plots ----------------------------------------------------
+
+coursing_success_viewshed <- predictorEffect("Viewshed_scale", fit_coursing_success) %>% 
+    as_tibble() %>% 
+    mutate(Viewshed_unscaled = Viewshed_scale * Viewshed_scale_list$scale + Viewshed_scale_list$center,
+           Success = "Successful",
+           Mode = "Coursing") 
+coursing_unsuccess_viewshed <- predictorEffect("Viewshed_scale", fit_coursing_unsuccess) %>% 
+    as_tibble() %>% 
+    mutate(Viewshed_unscaled = Viewshed_scale * Viewshed_scale_list$scale + Viewshed_scale_list$center,
+           Success = "Unsuccessful",
+           Mode = "Coursing") 
+stalking_success_viewshed <- predictorEffect("Viewshed_scale", fit_stalking_success) %>% 
+    as_tibble() %>% 
+    mutate(Viewshed_unscaled = Viewshed_scale * Viewshed_scale_list$scale + Viewshed_scale_list$center,
+           Success = "Successful",
+           Mode = "Stalking") 
+stalking_unsuccess_viewshed <- predictorEffect("Viewshed_scale", fit_stalking_unsuccess) %>% 
+    as_tibble() %>% 
+    mutate(Viewshed_unscaled = Viewshed_scale * Viewshed_scale_list$scale + Viewshed_scale_list$center,
+           Success = "Unsuccessful",
+           Mode = "Stalking") 
+sitandwait_success_viewshed <- predictorEffect("Viewshed_scale", fit_sitandwait_success) %>% 
+    as_tibble() %>% 
+    mutate(Viewshed_unscaled = Viewshed_scale * Viewshed_scale_list$scale + Viewshed_scale_list$center,
+           Success = "Successful",
+           Mode = "Sit-and-Wait") 
+sitandwait_unsuccess_viewshed <- predictorEffect("Viewshed_scale", fit_sitandwait_unsuccess) %>% 
+    as_tibble() %>% 
+    mutate(Viewshed_unscaled = Viewshed_scale * Viewshed_scale_list$scale + Viewshed_scale_list$center,
+           Success = "Unsuccessful",
+           Mode = "Sit-and-Wait") 
+viewshed_predict <- dplyr::bind_rows(coursing_success_viewshed,
+                                     coursing_unsuccess_viewshed,
+                                     stalking_success_viewshed,
+                                     stalking_unsuccess_viewshed,
+                                     sitandwait_success_viewshed,
+                                     sitandwait_unsuccess_viewshed)
+
+ggplot(viewshed_predict, aes(x = Viewshed_unscaled, y = fit))+
+    geom_line(aes(col = Success)) +
+    geom_ribbon(aes(ymin = lower, ymax = upper, fill = Success), alpha = 0.2) +
+    labs(y = "Probability of Use", x = "Viewshed") +
+    #scale_fill_manual(values = c("#d8b365", "#969696", "#4d938a")) +
+    #scale_color_manual(values = c("#d8b365", "#969696", "#4d938a")) +
+    theme_bw() +
+    facet_wrap(~ Mode, scales = "free")
+
+
+# ALL ---------------------------------------------------------------------
+
+all_predict <- dplyr::bind_rows(viewshed_predict,
+                                chaparral_predict,
+                                woodland_predict,
+                                road_predict,
+                                rugged_predict) %>% 
+    dplyr::select(-c(Viewshed_scale,
+                     Chaparral_120m_scale,
+                     Woodland_120m_scale,
+                     Road_Distance_scale,
+                     Ruggedness_scale)) %>% 
+    tidyr::pivot_longer(cols = c(Viewshed_unscaled,
+                                 Chaparral_120m_unscaled,
+                                 Woodland_120m_unscaled,
+                                 Road_Distance_unscaled,
+                                 Ruggedness_unscaled),
+                        names_to = "Covariate",
+                        values_to = "Covariate_value") %>% 
+    tidyr::drop_na()
+
+# scales not working right
+ggplot(all_predict, aes(x = Covariate_value, y = fit))+
+    geom_line(aes(col = Success)) +
+    geom_ribbon(aes(ymin = lower, ymax = upper, fill = Success), alpha = 0.2) +
+    labs(y = "Probability of Use", x = "Covariate") +
+    theme_bw() +
+    facet_wrap(Covariate ~ Mode, scales = "free", ncol = 3)
