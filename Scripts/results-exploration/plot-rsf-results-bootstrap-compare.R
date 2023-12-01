aa7fa0edeaabc786d6dd09e77d7ea0219ee19491
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2023-12-01 08:43:00 -0800

    Bootstrap RSFs for 95% CIs

---
+++ Scripts/results-exploration/plot-rsf-results-bootstrap-compare.R
@@ -0,0 +1,75 @@
+library(ggplot2)
+library(dplyr)
+library(viridis)
+library(gtable)
+library(lemon)
+
+#all_rsf_results_boot <- read.csv("Results/rsf-results-by-mode-success-Nov29.csv") %>%
+all_rsf_results_boot <- read.csv("Results/rsf-bootstrapping-results.csv") %>% 
+    dplyr::filter(Predictor != "Intercept") %>% 
+    dplyr::rename("Hunting Mode" = "Hunting.Mode") %>% 
+    dplyr::mutate(Method = "New")
+all_rsf_results <- read.csv("Results/rsf-results-by-mode-success.csv") %>% 
+    dplyr::filter(Predictor != "Intercept") %>% 
+    dplyr::rename("Hunting Mode" = "Hunting.Mode") %>% 
+    dplyr::filter(Harvest == "Yes") %>% 
+    dplyr::mutate(Method = "Original") 
+
+all_rsf_results <- bind_rows(all_rsf_results, all_rsf_results_boot)
+
+# without road distance
+all_rsf_results %>% 
+    dplyr::filter(Predictor != "Road_Distance") %>% 
+    dplyr::filter(Predictor != "Road Distance") %>% 
+    #ggplot(aes(x = Predictor, y = Coefficient, col = `Hunting Mode`, shape = Harvest, group = `Hunting Mode`)) + 
+    ggplot(aes(x = Predictor, y = Coefficient, col = `Hunting Mode`, shape = Method)) + 
+    geom_hline(aes(yintercept = 0), linetype="dashed", size = 0.5, color = "darkgrey") +
+    geom_errorbar(aes(ymin = LCI, ymax = UCI), width=0, position = position_dodge(width = 1), alpha=.5) +
+    geom_point(position = position_dodge(width = 1), size = 2, stroke = 1, alpha = .75) +
+    theme(axis.title.y = element_blank(),
+          legend.key = element_rect(fill = alpha("white", 0.0)),
+          strip.background = element_blank(),
+          strip.text = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.background = element_blank(),
+          axis.ticks.y = element_blank(),
+          axis.text.y = element_text(size=rel(1.25)),
+          panel.border = element_rect(color = "black", fill = NA, size = .5),
+          legend.text=element_text(size=rel(1))
+    ) +
+    scale_y_continuous(breaks=c(-1, -.75, -.5, -.25, 0, .25)) +
+    coord_flip() + # switch x and y coordinates
+    ylab("Coefficient") +
+    scale_shape_manual(values=c(1, 19))+
+    scale_color_manual(values = c("#1b9e77", "#d95f02", "#7570b3"),
+                       guide = guide_legend(reverse = TRUE)) +
+    facet_grid(Predictor~., scales = "free")
+#ggsave("Figures/rsf-coefficients-1-30min.pdf", width = 8, height = 4)
+
+# only road distance
+all_rsf_results %>% 
+    dplyr::filter(Predictor == "Road Distance") %>% 
+    #ggplot(aes(x = Predictor, y = Coefficient, col = `Hunting Mode`, shape = Harvest, group = `Hunting Mode`)) + 
+    ggplot(aes(x = Predictor, y = Coefficient, col = `Hunting Mode`, shape = Harvest)) + 
+    geom_hline(aes(yintercept = 0), linetype="dashed", size = 0.5, color = "darkgrey") +
+    geom_errorbar(aes(ymin = LCI, ymax = UCI), width=0, position = position_dodge(width = 1), alpha=.5) +
+    geom_point(position = position_dodge(width = 1), size = 2, stroke = 1, alpha = .75) +
+    theme(axis.title.y = element_blank(),
+          legend.key = element_rect(fill = alpha("white", 0.0)),
+          strip.background = element_blank(),
+          strip.text = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.background = element_blank(),
+          axis.ticks.y = element_blank(),
+          axis.text.y = element_text(size=rel(1.25)),
+          panel.border = element_rect(color = "black", fill = NA, size = .5),
+          legend.text=element_text(size=rel(1))
+    ) +
+    coord_flip() + # switch x and y coordinates
+    ylab("Coefficient") +
+    ylim(-5, 0.5) +
+    scale_shape_manual(values=c(1, 19))+
+    scale_color_manual(values = c("#1b9e77", "#d95f02", "#7570b3"),
+                       guide = guide_legend(reverse = TRUE)) +
+    facet_grid(Predictor~., scales = "free") 
+ggsave("Figures/rsf-coefficients-2-30min.pdf", width = 8.15, height = 1.3)
---
+++ for-publication/05-rsf-bootstrapping.R
@@ -0,0 +1,190 @@
+
+library(dplyr)
+library(jtools)
+library(lme4)
+library(boot)
+
+# Bring in all available points in study area
+available <- read.csv("for-publication/all-available-point-cov.csv")
+available$Used <- 0
+
+# Bring in used points
+used <- read.csv("for-publication/igotu_data_3min_covariates.csv")
+used$Used <- 1
+
+# Randomly select 100 points for each available point
+set.seed(123)
+counts <- count(used, ID)
+counts$n100 <- counts$n * 100
+available_dfs <- list()
+for(i in 1:nrow(counts)) {
+    available100 <- dplyr::sample_n(available, counts$n100[i]) 
+    available100$ID <- counts$ID[i]
+    available_dfs[[i]] <- available100
+}
+available_100 <- dplyr::bind_rows(available_dfs)
+
+# Join into single dataframe and scale covariates
+used_avail <- dplyr::bind_rows(used, available_100) %>% 
+    dplyr::select(ID, Ruggedness, Viewshed, Road_Distance, Chaparral_120m, Woodland_120m,
+                  Used) %>% 
+    dplyr::mutate(Ruggedness_scale = scale(Ruggedness),
+                  Viewshed_scale = scale(Viewshed),
+                  Road_Distance_scale = scale(Road_Distance),
+                  Chaparral_120m_scale = scale(Chaparral_120m),
+                  Woodland_120m_scale = scale(Woodland_120m))
+
+# Bring in clusters
+clusters <- read.csv("for-publication/hunter_cluster_success_long_nocov.csv") %>%
+    tidyr::pivot_wider(names_from = "State", values_from = "Percentage") %>% 
+    dplyr::select(ID, Cluster, Harvest)
+used_avail <- dplyr::left_join(used_avail, clusters)
+used_avail$ID <- as.character(used_avail$ID)
+
+# Split by cluster
+used_avail_coursing <- used_avail %>% 
+    dplyr::filter((Cluster != "Stalking" & Cluster != "Sit-and-wait"))
+used_avail_stalking <- used_avail %>% 
+    dplyr::filter((Cluster != "Coursing" & Cluster != "Sit-and-wait"))
+used_avail_sitandwait <- used_avail %>% 
+    dplyr::filter((Cluster != "Stalking" & Cluster != "Coursing"))
+
+# Filter by cluster & success
+used_avail_coursing_success <- used_avail_coursing %>% dplyr::filter(Harvest == "Y")
+used_avail_stalking_success <- used_avail_stalking %>% dplyr::filter(Harvest == "Y")
+used_avail_sitandwait_success <- used_avail_sitandwait %>% dplyr::filter(Harvest == "Y")
+used_avail_coursing_unsuccess <- used_avail_coursing %>% dplyr::filter(Harvest == "N")
+used_avail_stalking_unsuccess <- used_avail_stalking %>% dplyr::filter(Harvest == "N")
+used_avail_sitandwait_unsuccess <- used_avail_sitandwait %>% dplyr::filter(Harvest == "N")
+
+# Define your model function
+model_function <- function(data, indices) {
+    # Subset data using the bootstrap indices
+    boot_data <- data[indices, ]
+    
+    # Fit the logistic regression model
+    model <- glm(Used ~ Ruggedness_scale + Viewshed_scale + Chaparral_120m_scale + Woodland_120m_scale + Road_Distance_scale,
+                 data = boot_data,
+                 family = binomial)
+    
+    # Return the model coefficients
+    return(coef(model))
+}
+
+# Coursing successful
+set.seed(123)
+boot_coursing_success <- boot(data = used_avail_coursing_success, 
+                              statistic = model_function, R = 1000)
+boot_coefs_coursing_success <- boot_coursing_success$t %>% 
+    as.data.frame()
+names(boot_coefs_coursing_success) <- c("Intercept", "Ruggedness", "Viewshed", "Chaparral", "Woodland", "Road_Distance")
+write.csv(boot_coefs_coursing_success, "Results/rsf-bootstrap/boot_coursing_success.csv", row.names = FALSE)
+
+
+# Stalking successful
+set.seed(123)
+boot_stalking_success <- boot(data = used_avail_stalking_success, 
+                              statistic = model_function, R = 1000)
+boot_coefs_stalking_success <- boot_stalking_success$t %>% 
+    as.data.frame()
+names(boot_coefs_stalking_success) <- c("Intercept", "Ruggedness", "Viewshed", "Chaparral", "Woodland", "Road_Distance")
+write.csv(boot_coefs_stalking_success, "Results/rsf-bootstrap/boot_stalking_success.csv", row.names = FALSE)
+
+
+# Sit and wait successful
+set.seed(123)
+boot_sitandwait_success <- boot(data = used_avail_sitandwait_success, 
+                              statistic = model_function, R = 1000)
+boot_coefs_sitandwait_success <- boot_sitandwait_success$t %>% 
+    as.data.frame()
+names(boot_coefs_sitandwait_success) <- c("Intercept", "Ruggedness", "Viewshed", "Chaparral", "Woodland", "Road_Distance")
+write.csv(boot_coefs_sitandwait_success, "Results/rsf-bootstrap/boot_sitandwait_success.csv", row.names = FALSE)
+
+
+# Coursing unsuccessful
+set.seed(456)
+boot_coursing_unsuccess <- boot(data = used_avail_coursing_unsuccess, 
+                              statistic = model_function, R = 100)
+boot_coefs_coursing_unsuccess <- boot_coursing_unsuccess$t %>% 
+    as.data.frame()
+names(boot_coefs_coursing_unsuccess) <- c("Intercept", "Ruggedness", "Viewshed", "Chaparral", "Woodland", "Road_Distance")
+write.csv(boot_coefs_coursing_unsuccess, "Results/rsf-bootstrap/boot_coursing_unsuccess4.csv", row.names = FALSE)
+
+
+# Stalking ununsuccessful
+set.seed(123)
+boot_stalking_unsuccess <- boot(data = used_avail_stalking_unsuccess, 
+                              statistic = model_function, R = 1000)
+boot_coefs_stalking_unsuccess <- boot_stalking_unsuccess$t %>% 
+    as.data.frame()
+names(boot_coefs_stalking_unsuccess) <- c("Intercept", "Ruggedness", "Viewshed", "Chaparral", "Woodland", "Road_Distance")
+write.csv(boot_coefs_stalking_unsuccess, "Results/rsf-bootstrap/boot_stalking_unsuccess.csv", row.names = FALSE)
+
+
+# Sit and wait ununsuccessful
+set.seed(123)
+boot_sitandwait_unsuccess <- boot(data = used_avail_sitandwait_unsuccess, 
+                              statistic = model_function, R = 1000)
+boot_coefs_sitandwait_unsuccess <- boot_sitandwait_unsuccess$t %>% 
+    as.data.frame()
+names(boot_coefs_sitandwait_unsuccess) <- c("Intercept", "Ruggedness", "Viewshed", "Chaparral", "Woodland", "Road_Distance")
+write.csv(boot_coefs_sitandwait_unsuccess, "Results/rsf-bootstrap/boot_sitandwait_unsuccess.csv", row.names = FALSE)
+
+
+# Bring results back in
+boot_coefs_coursing_success <- read.csv("Results/rsf-bootstrap/boot_coursing_success.csv")
+boot_coefs_stalking_success <- read.csv("Results/rsf-bootstrap/boot_stalking_success.csv")
+boot_coefs_sitandwait_success <- read.csv("Results/rsf-bootstrap/boot_sitandwait_success.csv")
+
+coursing_success_mean <- boot_coefs_coursing_success %>% 
+    apply(2, mean,  na.rm = TRUE) %>% 
+    as.data.frame %>% 
+    tibble::rownames_to_column("Predictor")
+names(coursing_success_mean) <- c("Predictor", "Coefficient")
+coursing_success_CI <- boot_coefs_coursing_success %>% 
+    apply(2, quantile, probs = c(0.025, 0.975),  na.rm = TRUE) %>% 
+    t() %>% 
+    as.data.frame %>% 
+    tibble::rownames_to_column("Predictor") %>% 
+    dplyr::rename(LCI = `2.5%`, UCI = `97.5%`) %>% 
+    dplyr::left_join(coursing_success_mean) %>% 
+    dplyr::mutate(`Hunting Mode` = "Coursing",
+                  Harvest = "Yes",
+                  Model = paste(`Hunting Mode`, Harvest, sep = "_")) 
+
+stalking_success_mean <- boot_coefs_stalking_success %>% 
+    apply(2, mean,  na.rm = TRUE) %>% 
+    as.data.frame %>% 
+    tibble::rownames_to_column("Predictor")
+names(stalking_success_mean) <- c("Predictor", "Coefficient")
+stalking_success_CI <- boot_coefs_stalking_success %>% 
+    apply(2, quantile, probs = c(0.025, 0.975),  na.rm = TRUE) %>% 
+    t() %>% 
+    as.data.frame %>% 
+    tibble::rownames_to_column("Predictor") %>% 
+    dplyr::rename(LCI = `2.5%`, UCI = `97.5%`) %>% 
+    dplyr::left_join(stalking_success_mean) %>% 
+    dplyr::mutate(`Hunting Mode` = "Stalking",
+                  Harvest = "Yes",
+                  Model = paste(`Hunting Mode`, Harvest, sep = "_"))
+
+sitandwait_success_mean <- boot_coefs_sitandwait_success %>% 
+    apply(2, mean,  na.rm = TRUE) %>% 
+    as.data.frame %>% 
+    tibble::rownames_to_column("Predictor")
+names(sitandwait_success_mean) <- c("Predictor", "Coefficient")
+sitandwait_success_CI <- boot_coefs_sitandwait_success %>% 
+    apply(2, quantile, probs = c(0.025, 0.975),  na.rm = TRUE) %>% 
+    t() %>% 
+    as.data.frame %>% 
+    tibble::rownames_to_column("Predictor") %>% 
+    dplyr::rename(LCI = `2.5%`, UCI = `97.5%`) %>% 
+    dplyr::left_join(sitandwait_success_mean) %>% 
+    dplyr::mutate(`Hunting Mode` = "Sit-and-wait",
+                  Harvest = "Yes",
+                  Model = paste(`Hunting Mode`, Harvest, sep = "_"))
+
+all_success_CI <- dplyr::bind_rows(coursing_success_CI, 
+                                   stalking_success_CI, 
+                                   sitandwait_success_CI)
+write.csv(all_success_CI, "Results/rsf-bootstrapping-results.csv", row.names = FALSE)
