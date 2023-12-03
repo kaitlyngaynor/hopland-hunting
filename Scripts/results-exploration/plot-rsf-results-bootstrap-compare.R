c4555b9949b2dedffcf2cc14c9e9e5098b138111
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2023-12-03 11:49:15 -0800

    Make plots comparing bootstrapped and original CIs for response to reviewers

--- Scripts/results-exploration/plot-rsf-results-bootstrap-compare.R
+++ Scripts/results-exploration/plot-rsf-results-bootstrap-compare.R
@@ -4,15 +4,13 @@ library(viridis)
 library(gtable)
 library(lemon)
 
-#all_rsf_results_boot <- read.csv("Results/rsf-results-by-mode-success-Nov29.csv") %>%
 all_rsf_results_boot <- read.csv("Results/rsf-bootstrapping-results.csv") %>% 
     dplyr::filter(Predictor != "Intercept") %>% 
     dplyr::rename("Hunting Mode" = "Hunting.Mode") %>% 
-    dplyr::mutate(Method = "New")
+    dplyr::mutate(Method = "Bootstrapped")
 all_rsf_results <- read.csv("Results/rsf-results-by-mode-success.csv") %>% 
     dplyr::filter(Predictor != "Intercept") %>% 
     dplyr::rename("Hunting Mode" = "Hunting.Mode") %>% 
-    dplyr::filter(Harvest == "Yes") %>% 
     dplyr::mutate(Method = "Original") 
 
 all_rsf_results <- bind_rows(all_rsf_results, all_rsf_results_boot)
@@ -21,15 +19,14 @@ all_rsf_results <- bind_rows(all_rsf_results, all_rsf_results_boot)
 all_rsf_results %>% 
     dplyr::filter(Predictor != "Road_Distance") %>% 
     dplyr::filter(Predictor != "Road Distance") %>% 
-    #ggplot(aes(x = Predictor, y = Coefficient, col = `Hunting Mode`, shape = Harvest, group = `Hunting Mode`)) + 
-    ggplot(aes(x = Predictor, y = Coefficient, col = `Hunting Mode`, shape = Method)) + 
+    ggplot(aes(x = Predictor, y = Coefficient, col = `Hunting Mode`, shape = Harvest, group = `Hunting Mode`)) +
     geom_hline(aes(yintercept = 0), linetype="dashed", size = 0.5, color = "darkgrey") +
     geom_errorbar(aes(ymin = LCI, ymax = UCI), width=0, position = position_dodge(width = 1), alpha=.5) +
     geom_point(position = position_dodge(width = 1), size = 2, stroke = 1, alpha = .75) +
     theme(axis.title.y = element_blank(),
           legend.key = element_rect(fill = alpha("white", 0.0)),
           strip.background = element_blank(),
-          strip.text = element_blank(),
+          strip.text.y = element_blank(),
           panel.grid.major = element_blank(),
           panel.background = element_blank(),
           axis.ticks.y = element_blank(),
@@ -43,14 +40,12 @@ all_rsf_results %>%
     scale_shape_manual(values=c(1, 19))+
     scale_color_manual(values = c("#1b9e77", "#d95f02", "#7570b3"),
                        guide = guide_legend(reverse = TRUE)) +
-    facet_grid(Predictor~., scales = "free")
-#ggsave("Figures/rsf-coefficients-1-30min.pdf", width = 8, height = 4)
+    facet_grid(Predictor~Method, scales = "free")
 
 # only road distance
 all_rsf_results %>% 
-    dplyr::filter(Predictor == "Road Distance") %>% 
-    #ggplot(aes(x = Predictor, y = Coefficient, col = `Hunting Mode`, shape = Harvest, group = `Hunting Mode`)) + 
-    ggplot(aes(x = Predictor, y = Coefficient, col = `Hunting Mode`, shape = Harvest)) + 
+    dplyr::filter(Predictor %in% c("Road Distance", "Road_Distance")) %>% 
+    ggplot(aes(x = Method, y = Coefficient, col = `Hunting Mode`, shape = Harvest)) + 
     geom_hline(aes(yintercept = 0), linetype="dashed", size = 0.5, color = "darkgrey") +
     geom_errorbar(aes(ymin = LCI, ymax = UCI), width=0, position = position_dodge(width = 1), alpha=.5) +
     geom_point(position = position_dodge(width = 1), size = 2, stroke = 1, alpha = .75) +
@@ -72,4 +67,3 @@ all_rsf_results %>%
     scale_color_manual(values = c("#1b9e77", "#d95f02", "#7570b3"),
                        guide = guide_legend(reverse = TRUE)) +
     facet_grid(Predictor~., scales = "free") 
-ggsave("Figures/rsf-coefficients-2-30min.pdf", width = 8.15, height = 1.3)
